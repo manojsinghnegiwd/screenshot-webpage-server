f1d0e066f4cae42484a5f105a20af4c0dab8441c
Manoj Singh Negi <me@manojsinghnegi.com>
2017-04-14 23:40:48 +0530

    added take photo function & installed UUID

--- src/server.js
+++ src/server.js
@@ -1,12 +1,40 @@
 const express = require('express');
+const phantom = require('phantom');
 
 let app = express();
 let PORT = 3000;
+let initViewPort = {width: 1360, height: 768};
+
+const takePhoto = (url, viewport = initViewPort ) => {
+	return new Promise ((res, req) => {
+		let phInstance = null;
+
+		phantom.create()
+			.then(function (instance) {
+				phInstance = instance;
+				return instance.createPage();
+			})
+			.then(function (page) {
+				page.property('viewportSize', viewport).then(function() {
+					page.open(url)
+						.then(function () {
+							page.render('dummy.png');
+							phInstance.exit();
+							return res('success');
+						})
+				});
+			})
+			.catch(function (error) {
+				return rej(error);
+				phInstance.exit();
+			});
+	})
+} 
 
 app.get('/screenshot', (req, res) => {
-	res.json({
-		message: 'hello world'
-	})
+	takePhoto(req.query.url)
+		.then(msg => res.json({msg}))
+		.catch(msg => res.json({msg}))
 })
 
 // listen on port
