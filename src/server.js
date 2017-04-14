b82b96c3e6cbdfc6c89d9607b1df6d44c586d40e
Manoj Singh Negi <me@manojsinghnegi.com>
2017-04-14 23:07:17 +0530

    dummy server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
-console.log('Capturing Webpage');
+const express = require('express');
+
+let app = express();
+let PORT = 3000;
+
+app.get('/screenshot', (req, res) => {
+	res.json({
+		message: 'hello world'
+	})
+})
+
+// listen on port
+
+app.listen(PORT, () => console.log('Bring it on'))
