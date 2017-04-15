8ea639a9d3fa127410ecb6136c830fb75e427c70
Manoj Singh Negi <me@manojsinghnegi.com>
2017-04-16 01:36:14 +0530

    extended api to send images in base64 format

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const phantom = require('phantom');
 const uuidV4 = require('uuid/v4');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.NODE_ENV == 'production' ? 8080 : 3000 ;
@@ -46,6 +47,20 @@ const takePhoto = (url, viewport) => {
 	})
 }
 
+const convert_to_base64 = (path) => {
+
+	return new Promise ((res, rej) => {
+		fs.readFile(path, (err, data) => {
+			if(err) {
+				return rej(err);
+			}
+
+			return res(data.toString('base64'));
+		})
+	})
+
+}
+
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -58,13 +73,27 @@ app.use('/shots', express.static('assets'))
 
 app.get('/screenshot', (req, res) => {
 
-	let {url, width = initViewPort.width, height = initViewPort.height} = req.query
+	let {url, width = initViewPort.width, height = initViewPort.height, format = 'raw'} = req.query
 
 	takePhoto(url, {
 			width,
 			height
 		})
-		.then(name => res.status(200).sendFile(assets_dir + '/' + name))
+		.then(name => {
+			let path_to_file = assets_dir + '/' + name;
+			console.log(format)
+			switch (format) {
+				case 'raw':
+					return res.status(200).sendFile(path_to_file);
+				case 'base64':
+					return convert_to_base64(path_to_file)
+						.then(data => res.status(200).json({src: data}))
+						.catch(err => res.status(200).json(msg));
+				default:
+					return res.status(200).sendFile(path_to_file);
+
+			}
+		})
 		.catch(msg => res.status(400).json({msg}))
 })
 
