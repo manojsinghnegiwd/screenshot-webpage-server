c317fde0340ba3eda8207de9e673d6836d32322b
Manoj Singh Negi <me@manojsinghnegi.com>
2017-04-15 21:45:01 +0530

    added CORS

--- src/server.js
+++ src/server.js
@@ -46,6 +46,14 @@ const takePhoto = (url, viewport) => {
 	})
 }
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+})
+
 app.use('/shots', express.static('assets'))
 
 app.get('/screenshot', (req, res) => {
