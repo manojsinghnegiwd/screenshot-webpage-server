d63a9cee89c4e1d86ab06bdf30352b13447ef3c3
Manoj Singh Negi <me@manojsinghnegi.com>
2017-04-15 10:57:18 +0530

    now image comes as a response

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 const initViewPort = {width: 1360, height: 768};
-const assets_dir = './assets';
+const assets_dir = path.join(__dirname, '../assets');
 
 const generateName = () => (`${uuidV4()}.png`)
 
@@ -49,8 +49,8 @@ app.use('/shots', express.static('assets'))
 
 app.get('/screenshot', (req, res) => {
 	takePhoto(req.query.url)
-		.then(path => res.json({path}))
-		.catch(msg => res.json({msg}))
+		.then(name => res.status(200).sendFile(assets_dir + '/' + name))
+		.catch(msg => res.status(400).json({msg}))
 })
 
 // listen on port
